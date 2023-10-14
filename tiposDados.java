79853eb00a93af7badd581e653241533d84c79a5
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-10-14 13:35:17 -0300

    tipos de dados

---
+++ tiposDados.java
@@ -0,0 +1,16 @@
+public class tiposDados {
+    public static void main(String [] args){
+
+        System.out.println("Tipos de dados");
+        
+        byte tipoByte2 = Byte.MAX_VALUE; // 127
+        short tipoShort2 = Short.MAX_VALUE; // 32767
+        Integer tipoInteger2 = Integer.MAX_VALUE; // 2147483647
+        Long tipoLong1 = Long.MAX_VALUE; // 9223372036854775807L
+        Float tipoFloat = Float.MAX_VALUE; // 3.4028235E38F
+        Double tipoDouble = Double.MAX_VALUE; // 1.7976931348623157E308
+        String tipoString = "texto";
+        char tipoChar = 'S';
+        boolean tipoBoolean = true;
+    }
+}
